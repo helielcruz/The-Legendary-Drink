13d839fe9d8ecb55f90dba98ad87b1a9975f0a33
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-11 22:52:39 -0300

    feat: create CategoriesContainer component and add categories list request

---
+++ components/CategoriesContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <select v-model="selectedCategorie">
+            <option v-for="categorie of categories" :key="categorie" :value="categorie">{{ categorie }}</option>
+        </select>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { CocktailsRequests } from "../api/cocktail/requests/cocktails-requests";
+    
+    const selectedCategorie = ''
+
+    let categories = await new CocktailsRequests().categoriesList()
+    categories = categories.data.drinks.map((item: any) => item.strCategory)
+
+
+</script>
