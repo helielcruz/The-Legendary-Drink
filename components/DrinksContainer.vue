e8124a63c6722f28b72eba5f5e69168ae7b28342
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-13 00:42:04 -0300

    feat: create DrinksContainer

---
+++ components/DrinksContainer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <UContainer>
+            <UCard class="bg-transparent h-auto border-solid border-2 rounded-s-md bg-transparent pt-1 pb-1 border-violet-400">
+                <!-- <div v-for="drink of drinks" :key="drink">
+                    {{ drink }}
+                </div> -->
+                <UPagination
+                    v-model="drinks"
+                    :active-button="{ variant: 'outline' }"
+                    :inactive-button="{ color: 'gray' }"
+                    :model-value="1"
+                    :total="drinks.length"
+                />
+            </UCard>
+        </UContainer>
+    </div>
+</template>
+
+<script setup>
+    let drinks = useDrinks()
+</script>
