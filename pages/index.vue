e6370edb783b6244d298d4c0fe06aecd0e241b84
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-13 00:47:01 -0300

    refactor: insert components in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
-        <CategoriesContainer/>
+        <UContainer>
+            <FiltersContainer/>
+        </UContainer>
+        <DrinksContainer/>
+        <!-- {{ drinks }} -->
     </div>
 </template>
 
 <script lang="ts" setup>
-
+    const drinks = useDrinks()
 </script>
