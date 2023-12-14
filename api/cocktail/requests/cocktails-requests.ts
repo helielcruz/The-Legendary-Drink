5b5edab3d64810bf222e24748458d79f3294c9e2
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-13 23:04:45 -0300

    feat: add new request cocktailById

--- api/cocktail/requests/cocktails-requests.ts
+++ api/cocktail/requests/cocktails-requests.ts
@@ -12,4 +12,8 @@ export class CocktailsRequests {
     async cocktailByName (cocktail: string): Promise<any> {
         return await Cocktail.get(`/search.php?s=${cocktail}`)
     }
+
+    async cocktailById (cocktailId: string): Promise<any> {
+        return await Cocktail.get(`/lookup.php?i=${cocktailId}`)
+    }
 }
