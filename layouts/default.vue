4f585dbe33f64a1d85b2891ca99e4ed420593e2e
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-15 16:24:36 -0300

    feat: add image in layout default

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,8 @@
             <img class=" w-40" src="../assets/images/_c6668a1a-b7c3-48e5-be1e-67501f56d759-removebg-preview (1).png" alt="">
         </UContainer>
         <slot></slot>
+        <UContainer class="grid grid-cols-5 mt-2 place-items-center w-full bg-zinc-800">
+            <div class=" col-span-5">Developed by Heliel Cruz</div>
+        </UContainer>
     </div>
 </template>
