0d18b1b4aa5e0d51c3a78a15ffad9a74dc36e49d
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-14 23:33:15 -0300

    feat: add favorite composable

--- composables/states.ts
+++ composables/states.ts
@@ -1,3 +1,5 @@
 import type { Drinks } from "~/interfaces/drinks";
 
 export const useDrinks = () => useState<Drinks[]>("drinks", ()=> [])
+
+export const useFavorite = () => useState<Drinks[]>("favorites", ()=> [])
