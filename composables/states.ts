e5790ee76767e19d7b8e56b636aa45758fb1c6cf
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-15 16:40:53 -0300

    refacto: remove favorite composable

--- composables/states.ts
+++ composables/states.ts
@@ -1,5 +1,3 @@
 import type { Drinks } from "~/interfaces/drinks";
 
 export const useDrinks = () => useState<Drinks[]>("drinks", ()=> [])
-
-export const useFavorite = () => useState<Drinks[]>("favorites", ()=> [])
