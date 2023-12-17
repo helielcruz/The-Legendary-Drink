a6d286815670c0b0f42fadae6a236911293750b5
Heliel Cruz <heliel.cruz@loviz.com.vc>
2023-12-17 15:54:54 -0300

    refactor: change in validations axios error method

--- composables/useErrors.ts
+++ composables/useErrors.ts
@@ -1,24 +1,31 @@
+import { isAxiosError } from "axios"
+
 export default function () {
 
     const responseErrors: any = {
-        '400': '400 – Bad Request',
-        '401': '401 – Unauthorized',
-        '403': '403 – Forbidden',
-        '404': '404 – Not Found',
-        '500': '500 – Internal Server Error',
-        '501': '501 – Not Implemented',
-        '502': '502 – Bad Gateway',
-        '503': '503 – Service Unavailable',
-        '504': '504 – Gateway Time-out',
-        '505': '505 – HTTP Version Not Supported'
+        400: '400 – Bad Request',
+        401: '401 – Unauthorized',
+        403: '403 – Forbidden',
+        404: '404 – Not Found',
+        500: '500 – Internal Server Error',
+        501: '501 – Not Implemented',
+        502: '502 – Bad Gateway',
+        503: '503 – Service Unavailable',
+        504: '504 – Gateway Time-out',
+        505: '505 – HTTP Version Not Supported'
     }
 
-    const codeErros = ['400', '401', '403', '404', '500', '501', '502', '503', '504', '505']
+    const codeErros = [400, 401, 403, 404, 500, 501, 502, 503, 504, 505]
 
     const errorsVerify = (error: any) => {
         
-        const response = codeErros.filter((code: string) => error.status?.includes(code)).map((code) => responseErrors[code])
-        if(response.length !== 0) return response
+        let response: number[]  = []
+        if(isAxiosError(error))
+            response = codeErros.filter((code: number) => {
+            if(error.response?.status === code) return responseErrors[`${code}`]
+        })
+        
+        if(response.length) return response
         else return error.message
     }
 
